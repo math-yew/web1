583c4b65f5a5c8633e8a60a3effe7f0d56336e0c
Matt Mecham <math.u@hotmail.com>
2017-06-15 16:22:07 -0600

    git commit -m

--- public/js/content.js
+++ public/js/content.js
@@ -3,6 +3,6 @@ var firstImage = "carpet-1.jpg";
 
 var lastImage = ""
 
-var half = "Our flooring warehouse has a huge selection of in-stock carpet, vinyl, laminate, hardwood, and tile flooring. Choose from our large selection in-stock or special order that perfect color from our extensive sample racks. At Taylor Carpets we don’t just sell cheap carpet. We sell quality carpet for discount prices. Our carpet prices are the lowest in Utah County.";
+var half = "Our warehouse has an enormous selection of in-stock carpet, tile, laminate, hardwood, and vinyl flooring. Choose from our huge in-stock selection or special order a different color from our extensive catalog. At Calvin Carpets we don’t just sell affordable carpet, we sell high quality carpet at discount prices. You'll find our carpet to be at the lowest prices in the state.";
 
-var four = "Carpet USA has been in business since 1976. As the oldest flooring store in Middleton, we are dedicated to provide you with experienced service.";
+var four = "Calvin Carpets has been in business since 1976. As the oldest flooring store in Middleton, we are dedicated to provide you with experienced service.";
